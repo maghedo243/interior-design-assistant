3982063dbf838bf8a11ffa3e3058682d83c87cc9
Micah Aghedo <micahaghedo@gmail.com>
2025-11-03 20:33:01 -0500

    Did some tutorial-based things

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -5,20 +5,10 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
