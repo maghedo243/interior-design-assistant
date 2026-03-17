04eed172a71c73e61308a562f42172c9826a90a7
Micah Aghedo <micahaghedo@gmail.com>
2026-03-17 13:45:58 -0400

    Minor cleanup

--- app/app/(tabs)/suggestswipe.tsx
+++ app/app/(tabs)/suggestswipe.tsx
@@ -95,28 +95,8 @@ export default function SuggestScreen() {
             </Draggable>
            
             <Text style={styles.productName}>{currentProduct.name}</Text>
-            {/* <View style={styles.buttonContainer}>
-                <View style={styles.topRow}>
-                    <View style={styles.buttonWrapper}>
-                        <Button icon={(size) => (<Ionicons name="checkmark-sharp" size={size} color={"green"}/>)}
-                                label={"Like"} onPress={() => scroll("like")}/>
-                    </View>
-                    <View style={styles.buttonWrapper}>
-                        <Button icon={(size) => (<Ionicons name="close-sharp" size={size} color={"red"}/>)}
-                                label={"Dislike"} onPress={() => scroll("dislike")}/>
-                    </View>
-                </View>
-                <View style={styles.maybeWrapper}>
-                    <Button icon={(size) => (<Ionicons name="ellipse-outline" size={size} color={"gray"}/>)}
-                            label={"Maybe"} onPress={() => scroll("maybe")}/>
-                </View>
-            </View> */}
         </View>
     );
-}
-
-function Trigger(message: string){
-    console.log(message)
 }
 
 const styles = StyleSheet.create({
@@ -129,27 +109,9 @@ const styles = StyleSheet.create({
         width: "90%",
         marginTop: '20%'
     },
-    buttonContainer: {
-        marginTop: '5%',
-        alignSelf: 'center',
-        flexDirection: 'column',
-        gap: 5,
-        height: '10%',
-        width: '80%'
-    },
-    topRow: {
-        flexDirection: 'row',
-        width: '100%',
-        gap: 20,
-    },
-    buttonWrapper: {
-        flex: 1
-    },
-    maybeWrapper: {
-        alignSelf: 'center',
-        width: '50%',
-    },
     productName: {
-        color: 'white'
+        marginTop: '5%',
+        color: 'white',
+        textAlign: 'center'
     }
 });
